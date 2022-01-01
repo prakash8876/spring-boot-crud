3b820adb8c1d3e5508b1c23d502c82de8277498c
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-01-01 19:53:58 +0530

    application name changed

--- crud-api/src/main/java/com/example/demo/ProductManagerApplication.java
+++ crud-api/src/main/java/com/example/demo/ProductManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApiApplication {
+public class ProductManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudApiApplication.class, args);
+		SpringApplication.run(ProductManagerApplication.class, args);
 	}
 
 }
