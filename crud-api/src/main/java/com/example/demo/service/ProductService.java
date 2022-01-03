ef6eda05902ed73d60ab27614c915bc2cf4799e2
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-01-03 22:33:40 +0530

    crudi api completed

---
+++ crud-api/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repo.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+	
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll() {
+		LOGGER.info("fetching list of all products");
+		return repo.findAll();
+	}
+	
+	public void save(Product product) {
+		repo.save(product);
+		LOGGER.info("Product {} saved", product);
+	}
+	
+	public Product get(Long id) {
+		LOGGER.info("fetching product of id {}", id);
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		repo.deleteById(id);
+		LOGGER.info("deleted product of id {}", id);
+	}
+}
